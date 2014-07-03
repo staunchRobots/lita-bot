464f5391e212d859ddcdbad4467dac32220c63ea
Leonardo Bighetti <leonardo.bighetti@staunchrobots.com>
2014-07-03 11:17:25 -0300

    initial stuff, put the env variables in your rc to use it properly

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source "https://rubygems.org"
+
+gem "lita"
+gem "lita-hipchat"
+
+# Uncomment to use the HipChat adapter
+# gem "lita-hipchat"
+
+# Uncomment to use the IRC adapter
+# gem "lita-irc"
+
+# Add handlers to give Lita new functionality.
+gem "lita-excuses"
+gem "lita-karma"
+gem 'lita-forecast'
+gem "lita-gems"
+gem "lita-high-five"
+gem "lita-quote"
+gem "lita-pls"
+gem "lita-google"
+# For example:
+# gem "lita-google-images"
+# gem "lita-karma"
---
+++ lita_config.rb
@@ -0,0 +1,38 @@
+Lita.configure do |config|
+  # The name your robot will use.
+  config.robot.name = "Staunch Bot"
+
+  # The locale code for the language to use.
+  config.robot.locale = :en
+
+  # The severity of messages to log. Options are:
+  # :debug, :info, :warn, :error, :fatal
+  # Messages at the selected level and above will be logged.
+  config.robot.log_level = :debug
+
+  config.robot.alias = "!"
+  # An array of user IDs that are considered administrators. These users
+  # the ability to add and remove other users from authorization groups.
+  # What is considered a user ID will change depending on which adapter you use.
+  # config.robot.admins = ["1", "2"]
+
+  config.robot.admins = ["50014_337877@chat.hipchat.com"]
+  # The adapter you want to connect with. Make sure you've added the
+  # appropriate gem to the Gemfile.
+  config.robot.adapter          = :hipchat
+  config.adapter.debug          = false
+  config.adapter.jid            = ENV['STAUNCHBOT_JID']
+  config.adapter.password       = ENV['STAUNCHBOT_PASS']
+  config.adapter.rooms          = :all
+
+  ## Example: Set options for the Redis connection.
+  # config.redis.host = "127.0.0.1"
+  # config.redis.port = 1234
+
+  # Lita Forecast
+  config.handlers.forecast.api_key = ENV['STAUNCHBOT_FORECAST_APIKEY']
+
+  ## Example: Set configuration for any loaded handlers. See the handler's
+  ## documentation for options.
+  # config.handlers.some_handler.some_config_key = "value"
+end
